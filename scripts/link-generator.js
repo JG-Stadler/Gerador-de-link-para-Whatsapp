8a205a673f388be99d66ad6b9903f9341db8ce31
jg_stadler <joaogbstadler@gmail.com>
2026-02-18 11:30:36 -0300

    Estilos atualizados, funcionalidades adicionadas (Versão semi final)

--- scripts/link-generator.js
+++ scripts/link-generator.js
@@ -1,11 +1,29 @@
 const submitButton = document.getElementById("generate-link");
+const ShowLinkBox = document.getElementById('show-link');
+const CopyBTN = document.getElementById("copy-link");
 let link = "";
 
 submitButton.addEventListener('click',()=>{
     const countryCode = document.getElementById("country-code").value;
     const phoneNumber = document.getElementById("phone-number").value;
     const defaultMessage = document.getElementById("message").value;
+    if(phoneNumber == ""){
+        alert("Digite seu telefone antes de gerar o link");
+        return;
+    }
     
     link = `https://wa.me/${countryCode}${phoneNumber}?text=${defaultMessage}`;
-    console.log(link);
+    ShowLinkBox.innerText = link;
+});
+
+// Botão para copiar o link
+
+CopyBTN.addEventListener("click",()=>{
+    if (!link) {
+       alert("Gere o link primeiro!");
+    return;
+    }
+
+    navigator.clipboard.writeText(link);
+    alert("Link copiado!");
 });
